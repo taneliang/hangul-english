667853c96f9db03ad1bb1cc6f640f4cb2c7d4f2d
E-Liang Tan <eliang@eliangtan.com>
2016-12-18 11:31:40 +0700

    Reindent files with 4 spaces instead of 2

--- index.js
+++ index.js
@@ -40,73 +40,73 @@ var vowels = ["A","E","I","O","U", "W","Y", "IO", "EI", "IE", "IA", "OU", "AN", 
 var nonTailConsonants = ["J","V","X"];
 
 function engToHangul(engStr) {
-  var retStr = "";
-  var uppercaseStr = engStr.toUpperCase();
-  var hangulBlock = new HangulBlock();
+    var retStr = "";
+    var uppercaseStr = engStr.toUpperCase();
+    var hangulBlock = new HangulBlock();
 
-  for (var i = 0, len = uppercaseStr.length; i < len; i++) {
-    var englishChar = uppercaseStr[i];
+    for (var i = 0, len = uppercaseStr.length; i < len; i++) {
+        var englishChar = uppercaseStr[i];
 
-    // Use bigram if it's valid
-    if (i < len-1) {
-      var englishChar2 = uppercaseStr[i+1]; // i+1 < len
-      var bigram = englishChar + englishChar2;
-      if (bigram in englishToHangulMap) {
-        englishChar = bigram; // Bigram exists. Use bigram.
-        i++; // Don't iterate over the second letter (englishChar2)
-      }
+        // Use bigram if it's valid
+        if (i < len-1) {
+            var englishChar2 = uppercaseStr[i+1]; // i+1 < len
+            var bigram = englishChar + englishChar2;
+            if (bigram in englishToHangulMap) {
+                englishChar = bigram; // Bigram exists. Use bigram.
+                i++; // Don't iterate over the second letter (englishChar2)
+            }
+        }
+
+        // If we have a non-alphabet, add the current hangul char if it exists, then add the non-alphabet
+        if (!(englishChar in englishToHangulMap)) {
+            var hangul = hangulBlock.toHangul();
+            if (hangul != undefined) {
+                retStr += hangul;
+            }
+            hangulBlock.reset();
+            retStr += englishChar;
+            continue;
+        }
+
+        var isVowel = (vowels.indexOf(englishChar) != -1);
+        var hangulChar = englishToHangulMap[englishChar];
+
+        if (!hangulBlock.canAddCharacter(englishChar)) {
+            // Rule 7: If the third letter in a character is a consonant, and the next letter (The first of the next character) is a vowel, move the consonant from the first character to the second, so you don't have to use a placeholder.  Like so:    Instead of 탗오  (TAC - Placeholder+O), make it 타초  (TA - CO)
+            var tail = undefined;
+            if (isVowel && hangulBlock.tail != undefined) {
+                tail = hangulBlock.tail;
+                hangulBlock.tail = undefined;
+            }
+
+            var hangul = hangulBlock.toHangul();
+            if (hangul != undefined) {
+                retStr += hangul;
+            }
+            hangulBlock.reset();
+
+            // Rule 7
+            hangulBlock.lead = tail;
+        }
+
+        if (isVowel) {
+            hangulBlock.addVowel(hangulChar);
+        } else {
+            hangulBlock.addConsonant(hangulChar);
+        }
     }
 
-    // If we have a non-alphabet, add the current hangul char if it exists, then add the non-alphabet
-    if (!(englishChar in englishToHangulMap)) {
-      var hangul = hangulBlock.toHangul();
-      if (hangul != undefined) {
+    // Add last character if we've reached the end
+    var hangul = hangulBlock.toHangul();
+    if (hangul != undefined) {
         retStr += hangul;
-      }
-      hangulBlock.reset();
-      retStr += englishChar;
-      continue;
     }
 
-    var isVowel = (vowels.indexOf(englishChar) != -1);
-    var hangulChar = englishToHangulMap[englishChar];
-
-    if (!hangulBlock.canAddCharacter(englishChar)) {
-      // Rule 7: If the third letter in a character is a consonant, and the next letter (The first of the next character) is a vowel, move the consonant from the first character to the second, so you don't have to use a placeholder.  Like so:    Instead of 탗오  (TAC - Placeholder+O), make it 타초  (TA - CO)
-      var tail = undefined;
-      if (isVowel && hangulBlock.tail != undefined) {
-        tail = hangulBlock.tail;
-        hangulBlock.tail = undefined;
-      }
-
-      var hangul = hangulBlock.toHangul();
-      if (hangul != undefined) {
-        retStr += hangul;
-      }
-      hangulBlock.reset();
-
-      // Rule 7
-      hangulBlock.lead = tail;
-    }
-
-    if (isVowel) {
-      hangulBlock.addVowel(hangulChar);
-    } else {
-      hangulBlock.addConsonant(hangulChar);
-    }
-  }
-
-  // Add last character if we've reached the end
-  var hangul = hangulBlock.toHangul();
-  if (hangul != undefined) {
-    retStr += hangul;
-  }
-
-  return retStr;
+    return retStr;
 }
 
 function genhangul() {
-  var englishBox = document.getElementById('englishbox');
-  var hangulBox = document.getElementById('hangulbox');
-  hangulBox.value = engToHangul(englishBox.value);
+    var englishBox = document.getElementById('englishbox');
+    var hangulBox = document.getElementById('hangulbox');
+    hangulBox.value = engToHangul(englishBox.value);
 }
